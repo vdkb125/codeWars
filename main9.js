877bab2bc86c67f2f1c824e75b5f2c68c6f2c402
Victor Kirksey-Brown <vdkb125@aol.com>
2021-03-04 13:41:09 -0600

    repeat string solution

---
+++ main9.js
@@ -0,0 +1,12 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+//
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  let repeat = ""
+  for (let i = 0; i < n; i++){
+    repeat += s
+  }
+  return repeat
+}
