2f9c08469eb0bab6e4dd75ff648797dd8a00f458
vdkb125 <vdkb125@aol.com>
2021-03-15 12:01:27 -0500

    rock, paper, scissors solution

---
+++ main24.js
@@ -0,0 +1,14 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+//
+// Examples:
+
+const rps = (p1, p2) => {
+  if ((p1 === 'rock' && p2 === 'scissors') || (p1 === 'scissors' && p2 === 'paper') || (p1 === 'paper' && p2 === 'rock')){
+    return 'Player 1 won!'
+  } else if ((p2 === 'rock' && p1 === 'scissors') || (p2 === 'scissors' && p1 === 'paper') || (p2 === 'paper' && p1 === 'rock')){
+    return 'Player 2 won!'
+  } else if (p1 === p2){
+    return "Draw!"
+  }
+};
