5d082f897e8014e4695a7f80acc1fcb243f5b231
vdkb125 <vdkb125@aol.com>
2021-03-24 13:44:50 -0500

    Spin my sdrow solution

---
+++ main33.js
@@ -0,0 +1,25 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+
+// spinWords( "This is a test") => returns "This is a test"
+
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(w){
+    let wordArray = w.split(' ')
+    let spunWord = []
+    wordArray.map((word) => { 
+        word.length < 5 ? spunWord.push(word) : spunWord.push(word.split('').reverse().join(''))
+    })
+    return spunWord.join(' ')
+  }
+
+
+//   if (word.length < 5){
+//     spunWord.push(word)
+// } else if (word.length >= 5){
+//     spunWord.push(word.split('').reverse().join(''))
+// }
